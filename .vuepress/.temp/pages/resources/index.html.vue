4ee975f627606f6668052e18fd306faf34e9660c
gaoozi <gaoozi@126.com>
2024-02-01 21:51:39 +0800

    move vuepress to starlight

--- .vuepress/.temp/pages/resources/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><h1 id="各种资料" tabindex="-1"><a class="header-anchor" href="#各种资料" aria-hidden="true">#</a> 各种资料</h1>
-</div></template>
-
-
--- .vuepress/sidebar.ts
+++
@@ -1,9 +0,0 @@
-import { sidebar } from "vuepress-theme-hope";
-import { notes } from './routes/notes.mts'
-import { tools } from './routes/tools.mts'
-import { resources } from './routes/resources.mjs'
-import { contact } from './routes/contact.mts'
-
-export default sidebar({
-  '/resources/': resources,
-});
